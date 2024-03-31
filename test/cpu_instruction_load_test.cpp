76f6852831f883b19126a53930ad8ed37ab95421
beepsort <michelle.hamer25@gmail.com>
2024-03-31 21:24:30 +0200

    test: Add LD_r_absnn unit test

--- test/cpu_instruction_load_test.cpp
+++ test/cpu_instruction_load_test.cpp
@@ -5,8 +5,8 @@
 
 TEST(LD_r_r_test, LD_B_A) {
     CpuInitHelper helper;
-    uint8_t *src = helper.registers.A;
-    uint8_t *dest = helper.registers.B;
+    uint8_t* src = helper.registers.A;
+    uint8_t* dest = helper.registers.B;
     *src = 50;
     *dest = 0;
     CPU::LD_r_r({helper.registers, dest, src}).tick();
@@ -16,7 +16,7 @@ TEST(LD_r_r_test, LD_B_A) {
 
 TEST(LD_r_n_test, LD_A_d8) {
     CpuInitHelper helper;
-    uint8_t *dest = helper.registers.A;
+    uint8_t* dest = helper.registers.A;
     *dest = 0;
     helper.addressDispatcher.write(*helper.registers.PC + 1, 50);
     CPU::LD_r_n instr({helper.registers, dest, helper.addressDispatcher});
@@ -29,7 +29,7 @@ TEST(LD_r_absrr_test, LD_A_absHL) {
     CpuInitHelper helper;
     *helper.registers.HL = MEMORY::WRAM_LO;
     helper.addressDispatcher.write(*helper.registers.HL, 50);
-    uint8_t *dest = helper.registers.A;
+    uint8_t* dest = helper.registers.A;
     *dest = 0;
     CPU::LD_r_absrr instr(helper.registers, dest, helper.registers.HL, helper.addressDispatcher);
     instr.tick();
@@ -41,7 +41,7 @@ TEST(LD_r_absrr_test, LD_A_absHL_inc) {
     CpuInitHelper helper;
     *helper.registers.HL = MEMORY::WRAM_LO;
     helper.addressDispatcher.write(*helper.registers.HL, 50);
-    uint8_t *dest = helper.registers.A;
+    uint8_t* dest = helper.registers.A;
     *dest = 0;
     CPU::LD_r_absrr instr(helper.registers, dest, helper.registers.HL, helper.addressDispatcher, CPU::AddressMutOperation::INC);
     instr.tick();
@@ -54,7 +54,7 @@ TEST(LD_r_absrr_test, LD_A_absHL_dec) {
     CpuInitHelper helper;
     *helper.registers.HL = MEMORY::WRAM_LO;
     helper.addressDispatcher.write(*helper.registers.HL, 50);
-    uint8_t *dest = helper.registers.A;
+    uint8_t* dest = helper.registers.A;
     *dest = 0;
     CPU::LD_r_absrr instr(helper.registers, dest, helper.registers.HL, helper.addressDispatcher, CPU::AddressMutOperation::DEC);
     instr.tick();
@@ -66,7 +66,7 @@ TEST(LD_r_absrr_test, LD_A_absHL_dec) {
 TEST(LD_absrr_r_test, LD_absHL_A) {
     CpuInitHelper helper;
     *helper.registers.HL = MEMORY::WRAM_LO;
-    uint8_t *src= helper.registers.A;
+    uint8_t* src = helper.registers.A;
     *src = 50;
     CPU::LD_absrr_r instr(helper.registers, helper.registers.HL, src, helper.addressDispatcher);
     instr.tick();
@@ -85,4 +85,18 @@ TEST(LD_absrr_n_test, LD_absHL_d8) {
     instr.tick();
     EXPECT_EQ(helper.addressDispatcher.read(MEMORY::WRAM_LO), 50);
     EXPECT_EQ(helper.addressDispatcher.read(*helper.registers.HL), 50);
+}
+
+TEST(LD_r_absnn_test, LD_A_absD16) {
+    CpuInitHelper helper;
+    uint8_t* dest = helper.registers.A;
+    *dest = 0;
+    helper.addressDispatcher.write(*helper.registers.PC + 1 ,(uint8_t) MEMORY::WRAM_LO);
+    helper.addressDispatcher.write(*helper.registers.PC + 2 ,(uint8_t) (MEMORY::WRAM_LO >> 8));
+    helper.addressDispatcher.write(MEMORY::WRAM_LO, 50);
+    CPU::LD_r_absnn instr(helper.registers, dest, helper.addressDispatcher);
+    instr.tick();
+    instr.tick();
+    instr.tick();
+    EXPECT_EQ(*helper.registers.A, 50);
 }
