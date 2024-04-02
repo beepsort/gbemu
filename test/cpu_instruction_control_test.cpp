591de8db086432e594c6d05f5def8efa6fe9f95d
beepsort <michelle.hamer25@gmail.com>
2024-04-02 18:01:35 +0200

    test: Add DI instruction unit test

---
+++ test/cpu_instruction_control_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "cpu_instruction_control.h"
+#include "cpu_registers.h"
+#include "cpu_init_helper.h"
+
+TEST(DI_test, DisableInterrupts) {
+    CpuInitHelper helper;
+    helper.registers.IME = true;
+    CPU::DI({helper.registers}).tick();
+    EXPECT_EQ(helper.registers.IME, false);
+}
+
