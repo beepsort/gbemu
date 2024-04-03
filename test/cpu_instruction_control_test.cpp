06fb13d8e3b7cb66f96afbb6862dcffd72c6a88b
beepsort <michelle.hamer25@gmail.com>
2024-04-03 20:33:30 +0200

    test: Add RET instruction unit test

--- test/cpu_instruction_control_test.cpp
+++ test/cpu_instruction_control_test.cpp
@@ -16,3 +16,20 @@ TEST(EI_test, EnableInterrupts) {
     CPU::EI({helper.registers}).tick();
     EXPECT_EQ(helper.registers.IME, true);
 }
+
+TEST(RET_test, RetAddress) {
+    CpuInitHelper helper;
+    uint16_t* pc = helper.registers.PC;
+    uint16_t* sp = helper.registers.SP;
+    *pc = 0;
+    *sp = MEMORY::WRAM_LO;
+    helper.addressDispatcher.write(MEMORY::WRAM_LO, 0x34);
+    helper.addressDispatcher.write(MEMORY::WRAM_LO + 1, 0x12);
+    CPU::RET instr(helper.registers, helper.addressDispatcher);
+    instr.tick();
+    instr.tick();
+    instr.tick();
+    instr.tick();
+    EXPECT_EQ(*pc, 0x1234);
+    EXPECT_EQ(*sp, MEMORY::WRAM_LO + 2);
+}
