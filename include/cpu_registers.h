88afe87c017b4c7702f0ef7c98890ca277915420
beepsort <michelle.hamer25@gmail.com>
2024-04-12 15:20:26 +0200

    fix: CPU registers now correctly initialized to post ROM values

--- include/cpu_registers.h
+++ include/cpu_registers.h
@@ -13,19 +13,19 @@ namespace CPU
         static const uint8_t FLAG_HALFCARRY = 0b0100;
         static const uint8_t FLAG_CARRY = 0b1000;
     public:
-        uint16_t* AF = new uint16_t(0x0001);
-        uint8_t* A = (uint8_t*) AF;
-        uint8_t* FLAGS = (uint8_t*) AF + 1;
-        uint16_t* BC = new uint16_t(0x0000);
-        uint8_t* B = (uint8_t*) BC;
-        uint8_t* C = (uint8_t*) BC + 1;
-        uint16_t* DE = new uint16_t(0x0000);
-        uint8_t* D = (uint8_t*) DE;
-        uint8_t* E = (uint8_t*) DE + 1;
-        uint16_t* HL = new uint16_t(0x0000);
-        uint8_t* H = (uint8_t*) HL;
-        uint8_t* L = (uint8_t*) HL + 1;
-        uint16_t* SP = new uint16_t(0x0000);
+        uint16_t* AF = new uint16_t(0x0101);
+        uint8_t* A = (uint8_t*) AF + 1;
+        uint8_t* FLAGS = (uint8_t*) AF;
+        uint16_t* BC = new uint16_t(0x0013);
+        uint8_t* B = (uint8_t*) BC + 1;
+        uint8_t* C = (uint8_t*) BC;
+        uint16_t* DE = new uint16_t(0x00D8);
+        uint8_t* D = (uint8_t*) DE + 1;
+        uint8_t* E = (uint8_t*) DE;
+        uint16_t* HL = new uint16_t(0x014D);
+        uint8_t* H = (uint8_t*) HL + 1;
+        uint8_t* L = (uint8_t*) HL;
+        uint16_t* SP = new uint16_t(0xFFFE);
         uint16_t* PC = new uint16_t(0x0100);
         // Interupt Master Enable
         bool IME = true;
