c98f76f5f40afc6ad10ca03e81eba60620bd26c3
beepsort <michelle.hamer25@gmail.com>
2024-04-16 20:10:07 +0200

    doc: Add missing documentation for timer registers

--- include/gameboy/memory_io.h
+++ include/gameboy/memory_io.h
@@ -27,10 +27,37 @@ namespace GAMEBOY
          * Bit 7: 1 when a transfer is requested, or in progress
          */
         static const uint16_t SERIAL_CONTROL = 0xFF02;
-        // TODO
+        /*
+         * DIV register is the visible portion of the system counter
+         * The system counter increments every M-cycle and is 6 bits longer than DIV
+         * This means the DIV register increments once every 64 M-cycles
+         */
         static const uint16_t TIMER_REG_DIV = 0xFF04;
+        /*
+         * TIMA is incremented based on a combination of the system counter
+         * and the TAC register. The TAC register configures the frequency by selecting
+         * which bit from the system counter will be used to increment TIMA.
+         * When TIMA overflows a timer interrupt is requested, as well as it's
+         * value is reset to the value stored in the TMA register.
+         * Note this happens the M-cycle AFTER the overflow
+         */
         static const uint16_t TIMER_REG_TIMA = 0xFF05;
+        /*
+         * TMA is the value to reset TIMA to after an overflow.
+         * It is one of the two methods of controlling the frequency
+         * of timer interrupts
+         */
         static const uint16_t TIMER_REG_TMA = 0xFF06;
+        /*
+         * TAC is the timer control register and controls whether TIMA is
+         * incremented, as well as the frequency of those increments if enabled.
+         * Bit 2: TIMA increment enable
+         * Bit 1&0: TIMA increment frequency, where:
+         * - 00: 256 M-cycles
+         * - 01: 4 M-cycles
+         * - 10: 16 M-cycles
+         * - 11: 64 M-cycles
+         */
         static const uint16_t TIMER_REG_TAC = 0xFF07;
         /*
          * 0xFF0F Interrupt Flag
