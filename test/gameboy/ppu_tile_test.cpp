4a08eba76b410e2d6a88eb29c96c5c9987d4a11c
beepsort <michelle.hamer25@gmail.com>
2024-04-22 11:34:13 +0200

    test: Add BG single line rendering test

--- test/gameboy/ppu_tile_test.cpp
+++ test/gameboy/ppu_tile_test.cpp
@@ -2,39 +2,50 @@
 #include "gameboy/ppu_tile.h"
 #include "cpu_init_helper.h"
 
-TEST(PPU_Tilemap_test, Debug) {
+TEST(PPU_Tilemap_test, BgSingleLine) {
     CpuInitHelper helper;
     // set bit 7 to enable PPU
     // set bit 4 to operate in unsigned 0x8000 base mode
     // unset bit 3 to map BG from 0x9800
-    helper.addressDispatcher.write(GAMEBOY::IOHandler::PPU_REG_LCDC, 0x90);
+    // set bit 0 to enable BG
+    helper.addressDispatcher.write(GAMEBOY::IOHandler::PPU_REG_LCDC, 0x91);
+    // set BG palette so 3=3, 2=2, 1=1, 0=0
     helper.addressDispatcher.write(0xFF47, 0xE4);
     // init tiles
     for (uint16_t tile_num = 0; tile_num<=0xFF; tile_num++)
     {
         uint16_t tile_start = GAMEBOY::VRAM_LO + tile_num * 16;
+        bool lo = true;
         for (int i=0; i<16; i++)
         {
-            helper.addressDispatcher.write(tile_start+i, 0x55);
+            if (lo)
+            {
+
+                helper.addressDispatcher.write(tile_start+i, 0x33);
+            }
+            else
+            {
+                helper.addressDispatcher.write(tile_start+i, 0x55);
+            }
+            lo = !lo;
         }
     }
     // init map
     for (uint16_t map_index = 0; map_index < 0x400; map_index++)
     {
         uint16_t map_base_addr = 0x9800;
-        helper.addressDispatcher.write(map_base_addr+map_index, (uint8_t)map_index);
+        helper.addressDispatcher.write(map_base_addr+map_index, 0);
     }
     GAMEBOY::PPU_Tilemap tilemap(helper.addressDispatcher);
     auto line = tilemap.render_line(
             GAMEBOY::PPU_Tilemap::MAP_SELECT::MAP0,
-            55,
-            55,
-            9);
+            8,
+            0,
+            0);
     for (size_t i=0; i<line->size(); i++)
     {
-        printf("%X ", (*line)[i]);
+        ASSERT_EQ((*line)[i], i%4);
     }
-    printf("\n");
 }
 
 TEST(PPU_Tile_test, AllUnsigned) {
