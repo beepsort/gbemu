9fb2e7f59637dd41698ac30b1eaa797a47647e54
beepsort <michelle.hamer25@gmail.com>
2024-04-22 16:06:44 +0200

    test: Extend PPU state test to test memory locking

--- test/gameboy/ppu_test.cpp
+++ test/gameboy/ppu_test.cpp
@@ -13,22 +13,38 @@ TEST(PPU_test, StateMachine) {
         {
             for (int j=0; j<80; j++)
             {
+                helper.addressDispatcher.write(GAMEBOY::VRAM_LO, 0x11);
+                EXPECT_EQ(helper.addressDispatcher.read(GAMEBOY::VRAM_LO), 0x11);
+                helper.addressDispatcher.write(GAMEBOY::OAM_LO, 0x00);
+                EXPECT_NE(helper.addressDispatcher.read(GAMEBOY::OAM_LO), 0x00);
                 EXPECT_EQ(ppu.mode_no(), 2);
                 ppu.tick();
             }
             for (int j=0; j<172; j++)
             {
+                helper.addressDispatcher.write(GAMEBOY::VRAM_LO, 0x00);
+                EXPECT_NE(helper.addressDispatcher.read(GAMEBOY::VRAM_LO), 0x00);
+                helper.addressDispatcher.write(GAMEBOY::OAM_LO, 0x00);
+                EXPECT_NE(helper.addressDispatcher.read(GAMEBOY::OAM_LO), 0x00);
                 EXPECT_EQ(ppu.mode_no(), 3);
                 ppu.tick();
             }
             for (int j=0; j<204; j++)
             {
+                helper.addressDispatcher.write(GAMEBOY::VRAM_LO, 0x33);
+                EXPECT_EQ(helper.addressDispatcher.read(GAMEBOY::VRAM_LO), 0x33);
+                helper.addressDispatcher.write(GAMEBOY::OAM_LO, 0x33);
+                EXPECT_EQ(helper.addressDispatcher.read(GAMEBOY::OAM_LO), 0x33);
                 EXPECT_EQ(ppu.mode_no(), 0);
                 ppu.tick();
             }
         }
         for (int i=0; i<4560; i++)
         {
+            helper.addressDispatcher.write(GAMEBOY::VRAM_LO, 0x44);
+            EXPECT_EQ(helper.addressDispatcher.read(GAMEBOY::VRAM_LO), 0x44);
+            helper.addressDispatcher.write(GAMEBOY::OAM_LO, 0x44);
+            EXPECT_EQ(helper.addressDispatcher.read(GAMEBOY::OAM_LO), 0x44);
             EXPECT_EQ(ppu.mode_no(), 1);
             ppu.tick();
         }
