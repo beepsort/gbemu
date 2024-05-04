2844f27ee4cbe8490dbfac46d3ab1be356a6f240
beepsort <michelle.hamer25@gmail.com>
2024-05-04 02:01:43 +0200

    test: Add oam line rendering unit test for single sprite

--- test/gameboy/ppu_sprite_test.cpp
+++ test/gameboy/ppu_sprite_test.cpp
@@ -111,6 +111,53 @@ TEST(PPU_Sprite_test, ArrowSprite) {
     }
 }
 
+TEST(PPU_OamEntry_test, ArrowSprite) {
+    CpuInitHelper helper;
+    // set bit 7 to enable PPU
+    // set bit 1 to enable sprites
+    helper.addressDispatcher.write(GAMEBOY::IOHandler::PPU_REG_LCDC, 0x82);
+    // set OAM0 palette so 3=3, 2=2, 1=1
+    helper.addressDispatcher.write(0xFF48, 0xE4);
+    // init tile 0
+    uint16_t tile_start = GAMEBOY::VRAM_LO;
+    uint8_t sprite_bytes[] = {0x00, 0x00, 0x60, 0x60, 0x70, 0x70, 0x78, 0x78, 0x78, 0x78, 0x70, 0x70, 0x60, 0x60, 0x00, 0x00};
+    for (int i=0; i<16; i++)
+    {
+        helper.addressDispatcher.write(tile_start+i, sprite_bytes[i]);
+    }
+    // init oam
+    // y position
+    helper.addressDispatcher.write(GAMEBOY::OAM_LO, 16);
+    // x position
+    helper.addressDispatcher.write(GAMEBOY::OAM_LO+1, 8);
+    // tile index
+    helper.addressDispatcher.write(GAMEBOY::OAM_LO+2, 0);
+    // oam attrs
+    helper.addressDispatcher.write(GAMEBOY::OAM_LO+3, 0);
+    GAMEBOY::PPU_OamEntry oam0(0, helper.addressDispatcher);
+    GAMEBOY::PPU_Spritecache sprite_cache(helper.addressDispatcher);
+    uint8_t sprite_render_bytes[] = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 3, 3, 0, 0, 0, 0, 0,
+        0, 3, 3, 3, 0, 0, 0, 0,
+        0, 3, 3, 3, 3, 0, 0, 0,
+        0, 3, 3, 3, 3, 0, 0, 0,
+        0, 3, 3, 3, 0, 0, 0, 0,
+        0, 3, 3, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    };
+    for (uint8_t line=0; line<8; line++)
+    {
+        auto line_buffer = std::make_shared<GAMEBOY::LINE_PIXELS>();
+        oam0.render_line(line, line_buffer, sprite_cache);
+        for (size_t x=0; x<8; x++)
+        {
+            EXPECT_EQ((*line_buffer)[x], sprite_render_bytes[line*8+x]);
+        }
+    }
+}
+
+
 TEST(PPU_test, SpriteIntegration) {
     CpuInitHelper helper;
     // set bit 7 to enable PPU
